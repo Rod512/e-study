d326cd8db21475b55a0c77aebb282ec8f7bdf8be
Rod512 <rudro149187@bsdi-bd.org>
2023-12-16 00:03:07 +0530

    com: navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        brightBackground: '#FDFBEE',
+        brightGreen: "#539165",
+        lightGreen: "#959595"
+      }
+    },
   },
   plugins: [],
 }
